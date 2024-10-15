9096166c3a3773a00bc3553fc756f673d4971568
Namoux <quoc-nam.ngo@laplateforme.io>
2024-10-15 11:58:40 +0200

    Rajout sleep

--- pileouface.c
+++ pileouface.c
@@ -2,6 +2,7 @@
 #include <string.h>
 #include <stdlib.h>
 #include <time.h>
+#include <unistd.h>
 #define BUF_SIZE 255
 
 
@@ -28,12 +29,19 @@ int main () {
 
     if (reponse == 1) {
         printf("Vous faites Pile !\n");
-        printf("La pièce est jetée...\n");
+        printf("La pièce est jetée");
             srand(time(NULL));
             float from = 1;
             float to = 2;
 
             int random = (float)rand()/RAND_MAX * (to-from+1) + from;
+            for(int i = 0; i < 3; i++) {
+                printf(".");
+                // printf est bufferisé et qu'il n'est pas vidé à chaque appel, il faut le forcer. L'utilisation de '\n' force (en général) force l'écriture. 
+                fflush(stdout);
+                usleep(500000);
+            }
+            printf("\n");
 
             if (random == 1) { 
                 printf("PILE!\n");
@@ -59,12 +67,19 @@ int main () {
 
     } else if (reponse == 2) {
         printf("Vous faites Face !\n");
-        printf("La pièce est jetée...\n");
+        printf("La pièce est jetée");
             srand(time(NULL));
             float from = 1;
             float to = 2;
 
             int random = (float)rand()/RAND_MAX * (to-from+1) + from;
+            for(int i = 0; i < 3; i++) {
+                printf(".");
+                // printf est bufferisé et qu'il n'est pas vidé à chaque appel, il faut le forcer. L'utilisation de '\n' force (en général) force l'écriture. 
+                fflush(stdout);
+                usleep(500000);
+            }
+            printf("\n");
 
             if (random == 1) { 
                 printf("PILE!\n");

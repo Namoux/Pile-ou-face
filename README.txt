e8ec721f49c5605e3d1ec5bb10280391ff09e5c3
Namoux <quoc-nam.ngo@laplateforme.io>
2024-10-17 14:26:31 +0200

    Bonus

---
+++ README.txt
@@ -0,0 +1,49 @@
+Bonjour à tous,
+
+Présentation personnelle : Je m'appelle Nam, et je suis actuellement en formation DWWM.
+
+Synopsis : Je vais vous présenter mon projet qui consiste en un jeu simple et amusant : "Pile ou Face". L'objectif de ce projet était de créer une application interactive où les utilisateurs peuvent deviner le résultat d'un lancer de pièce.
+
+Cas d'utilisation : Avec mon application "Pile ou Face", l'utilisateur peut :
+
+    Choisir entre "Pile" et "Face".
+
+    Voir le résultat du lancer de pièce.
+
+    Suivre ses réussites et ses échecs.
+
+    Quitter le jeu à tout moment.
+
+Stack technique : Pour réaliser ce projet, j'ai utilisé les technologies suivantes :
+
+    Langage de programmation : C.
+
+    Compilateur : GCC.
+
+    Système d'exploitation : Debian 12.
+
+    Environnement de développement : Visual Studio Code.
+
+Code : Le projet se compose principalement de deux parties :
+
+    La fonction saisie() :
+
+        Elle gère les entrées de l'utilisateur, vérifie les choix et traite les saisies en majuscules et minuscules.
+
+    La fonction main() :
+
+        Elle lance le jeu, gère les lancers de pièce et maintient le décompte des réussites et des échecs.
+
+Difficultées : Pendant le développement de ce projet, j'ai rencontré plusieurs difficultés, notamment :
+
+    La gestion des entrées de l'utilisateur, en particulier pour traiter les majuscules et les minuscules.
+
+    L'utilisation de la fonction atoi et la vérification correcte des entrées numériques et textuelles.
+
+    L'implémentation d'un générateur de nombres aléatoires pour simuler le lancer de pièce.
+
+Améliorations futures : Pour les futures versions de ce projet, je prévois les améliorations suivantes :
+
+    Utiliser plus de fonctions pour eviter de surcharger le main.
+
+Au revoir : Voilà, c'était la présentation de mon projet "Pile ou Face". Merci de m'avoir écouté et je suis prêt à répondre à vos questions.
--- pileouface.c
+++ pileouface.c
@@ -5,41 +5,77 @@
 #include <unistd.h>
 #define BUF_SIZE 255
 
+// Fonction conversion en minuscules
+void conversmajmin(char *str) { 
+    for (int i = 0; i < strlen(str); i++) {
+        if (str[i] >= 65 && str[i] <= 90) {
+            str[i] = str[i] + 32;  
+        }
+    }
+}
+
+// Fonction entrée utilisateur
+int saisie() {
+    char buf[BUF_SIZE];
+    memset(buf, 0, BUF_SIZE);  // Initialisation de buf
+
+    printf("\n");
+    printf("Pile ou face ?\n");
+    printf("1. Pile\n");
+    printf("2. Face\n");
+    printf("0. Quitter le jeu\n");
+    printf("\n");
+
+    fgets(buf, BUF_SIZE, stdin);
+          buf[strlen(buf)-1] = 0;  // Supprimer la touche entrée '\n'
+
+    int numero = atoi(buf);  // Utilisation de atoi pour convertir buf en int
+
+    if (numero != 0 || strcmp(buf, "0") == 0) {  // Vérification si la saisie est réellement "0"
+        return numero;  // Si la saisie est 1, 2 ou 0, on la retourne directement
+    }
+
+    // Conversion de la chaîne en minuscules
+    conversmajmin(buf);
+
+    // Comparaison de l'entrée et conversion valeur en int 
+    if (strcmp(buf, "pile") == 0 || strcmp(buf, "p") == 0) {
+        return 1; 
+    } else if (strcmp(buf, "face") == 0 || strcmp(buf, "f") == 0) {
+        return 2; 
+    } else if (strcmp(buf, "quit") == 0 || strcmp(buf, "q") == 0) {
+        return 0; 
+    }
+
+    return -1;  // Erreur si la saisie n'est pas reconnue
+}
+
 int main () {
 
-    char buf[BUF_SIZE];
-    memset(buf, 0, BUF_SIZE);
     int reponse = 0;
     int reussite = 0;
     int echec = 0;
 
-    //titre du jeu
     printf("######## Pile ou Face ########\n");
     printf("\n");
 
     do {
-        printf("\n");
-        printf("Pile ou face ?\n"); 
-        printf("1. Pile\n");
-        printf("2. Face\n");
-        printf("0. Quitter le jeu\n");
-        printf("\n");
 
-        fgets(buf, BUF_SIZE, stdin);
-        buf[strlen(buf)-1] = 0;
-        reponse = atoi(buf);
+        reponse = saisie();
         
         if (reponse == 1) {
             printf("Vous faites Pile !\n");
             printf("La pièce est jetée");
-                srand(time(NULL));
+                    // random un chiffre entre 1 et 2
+                srand(time(NULL)); 
                 float from = 1;
                 float to = 2;
 
                 int random = (float)rand()/RAND_MAX * (to-from+1) + from;
+                // 3 petits points pour le suspense
                 for(int i = 0; i < 3; i++) {
                     printf(".");
-                    // printf est bufferisé et qu'il n'est pas vidé à chaque appel, il faut le forcer. L'utilisation de '\n' force (en général) force l'écriture. 
+                    // printf est bufferisé et qu'il n'est pas vidé à chaque appel, il faut le forcer. L'utilisation de '\n' force (en général) l'écriture. 
                     fflush(stdout);
                     usleep(500000);
                 }
@@ -74,11 +110,13 @@ int main () {
         } else if (reponse == 2) {
             printf("Vous faites Face !\n");
             printf("La pièce est jetée");
+                // random un chiffre entre 1 et 2
             srand(time(NULL));
             float from = 1;
             float to = 2;
 
             int random = (float)rand()/RAND_MAX * (to-from+1) + from;
+            // 3 petits points pour le suspense
             for(int i = 0; i < 3; i++) {
                 printf(".");
                 // printf est bufferisé et qu'il n'est pas vidé à chaque appel, il faut le forcer. L'utilisation de '\n' force (en général) force l'écriture. 
@@ -116,7 +154,7 @@ int main () {
         } else if (reponse == 0) {
             printf("Vous quittez le jeu.\n");
         }
-
+            // singulier pluriel
         if (reussite <= 1) {
         printf("Réussite : %d\n", reussite);
         } else { printf("Réussites : %d\n", reussite); }
@@ -134,7 +172,7 @@ int main () {
         if (reponse == 0) {
             printf("########### GAME OVER ###########\n");
         }
-
+        // Boucle while pour que le jeu ne s'arrete pas au 1er tour
     } while (reponse!= 0 && echec < 10);
 
     return 0;
